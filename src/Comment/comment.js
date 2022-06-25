5aa7076cb4c83629bf7b3f146bd6a4c8c5fdd75e
haomapu <haoanh7702@gmail.com>
2022-06-25 19:19:22 +0700

    fix bug and add cmt by react

--- src/Comment/comment.js
+++ src/Comment/comment.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 import "./comment.css"
 import Ava from './heof.jpg'
 import AllComments from '../allComments/allComments'
+import { v4 } from 'uuid'
 
-export default function comment() {
+export default function Comment() {
+  const [cmt, setCmt] = useState([])
+  const [textInput, setTextInput] = useState("");
+  const onTextInputchange = (e) => {
+    setTextInput(e.target.value);
+  }  
+  const onClickBtn = (e) => {
+    setCmt([...cmt, {id: v4(), name: textInput, isCompleted: false}]) 
+  }
   return (
     <div class="all">
       <div class="comment">
         <img class="ava" src={Ava} alt=""></img>
-        <input class="cmt" placeholder="Write the comment..."></input>
-        <button class="postBut">Post</button>
+        <input class="cmt" placeholder="Write the comment..."  onChange={onTextInputchange}></input>
+        <button class="postBut" onClick={onClickBtn}>Post</button>
       </div>
-        <AllComments />
-        <AllComments />
-        <AllComments />
+        <AllComments cmt={cmt}/>
+        <AllComments cmt={textInput}/>
     </div>
   )
 }
--- src/allComments/allComments.js
+++ src/allComments/allComments.js
@@ -2,13 +2,13 @@ import React from 'react'
 import "./allComments.css"
 import Ava from './heof.jpg'
 
-export default function allComments() {
+export default function allComments({cmt}) {
   return (
     <div class="allCmted">
       <img class="avacmted" src={Ava} alt=""></img>
       <div class="user">
         <span class="username">Hao Pham</span>
-        <span class="userCmt">This is a very good book</span>
+        <span class="userCmt">{cmt}</span>
       </div>
     </div>
   )
